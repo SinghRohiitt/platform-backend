bea2afb41188e1da5c73ea430f61789d59a1382d
user.name like : "SinghRohiitt <user.email like : "singhrohit0004008@gmail.com>
2025-09-09 01:12:53 +0530

    tesiting the middlewares

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {  signin, Signup } from "./auth.controller.js";
+import { signin, Signup } from "./auth.controller.js";
+import { authorizeRoles, isAuthenticated } from "../../middlewares/auth.js";
 
 const router = Router();
 
 // Routes
-router.post("/signup", Signup);
-router.post("/signin", signin);
+router.post("/signup", isAuthenticated, Signup);
+router.post("/signin", isAuthenticated, authorizeRoles("ADMIN"), signin);
 
 export default router;
