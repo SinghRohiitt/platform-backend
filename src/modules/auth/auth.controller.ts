41576cbf370b7ec47e82a1ff429d912a4b4c7496
Rohit Singh <singhrohit004008@gmail.com>
2025-12-09 14:23:01 +0530

    adding frontend domain name

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -56,7 +56,8 @@ export const signin = async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
+      domain: "13.60.31.21",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
